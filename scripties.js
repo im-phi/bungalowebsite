578375eec60bf21e0a68cf390b25f9beced0add7
Phi Hoang <pgh30@scarletmail.rutgers.edu>
2019-07-12 23:42:26 -0400

    added url ending to ajax request

--- scripties.js
+++ scripties.js
@@ -1,10 +1,3 @@
-$( document ).ready(function() {
-    $("#about").click(function() {
-            $("#main-content").load( "about.html #main-content", function() {
-        });
-    });
-});
-
 ;(function() {
     var throttle = function(type, name, obj) {
         var obj = obj || window;
@@ -29,4 +22,59 @@ window.addEventListener("optimizedScroll", function() {
         $("#background").css('-webkit-transform','rotate('+($(window).scrollTop()/8)+'deg)');
     }
 });
+
+/* _____________________________________ */
+/* _____________________________________ */
+/* __________NAVBAR SCRIPTS_____________ */
+/* _____________________________________ */
+
+window.onload = function(){
+    var nav_container = document.querySelector('.navbar ul');
+    
+    nav_container.addEventListener('click', function(e) {
+        if (e.target != e.currentTarget) {
+            e.preventDefault();
+            // e.target is the link inside the navbar we just clicked.
+            var data = e.target.getAttribute('id'),
+            url = data + ".html";
+            $("#main-content").load(url +" #main-content", function() {
+                window.history.pushState(data, null, url);
+            });
+        }
+        e.stopPropagation();
+    }, false);
+
+    window.addEventListener('popstate', function(e) {
+        // e.state is equal to the data-attribute of the last image we clicked
+        var instance = e.state;
+
+        if (instance == null) {
+            textWrapper.innerHTML = " ";
+            content.innerHTML = " ";
+            document.title = defaultTitle;
+        } else {
+            updateText(instance);
+            requestContent(instance + ".html");
+            document.title = "Bungalo | " + instance;
+        }
+    });
+}
+
+function requestContent(file) {
+    $('.#main-content').load(file + ' #main-content');
+}
+
+/* _____________________________________ */
+/* _____________________________________ */
+/* __________ABOUT PAGE SCRIPTS_________ */
+/* _____________________________________ */
+
+// $( document ).ready(function() {
+//     $("#about").click(function() {
+//             $("#main-content").load( "about.html #main-content", function() {
+//                 window.history.pushState('html', 'about', '/about.html');
+//             }
+//         );
+//     });
+// });
 
