ae9507ecb9ed20a1ed5bf394e98e267559b9f09d
Phi Hoang <pgh30@scarletmail.rutgers.edu>
2020-04-21 20:06:15 -0400

    site update

--- animation-script.js
+++
@@ -1,7 +0,0 @@
- var animation = bodymovin.loadAnimation({
-	 container: document.getElementById('circle-animation'),
-	 renderer: 'svg',
-	 loop: true,
-	 autoplay: true,
-	 path: 'circle-animation-data.json'
- });
